fdd3724ffe3b02abfa99f4412cc5fed5c128aca3
Marcel Krčah <5685134+mkrcah@users.noreply.github.com>
2021-12-31 15:51:31 +0100

    Maintenance

--- run-with-fzf.sh
+++ run-with-fzf.sh
@@ -7,7 +7,7 @@ set -e  # fail on error
 # where generated reports will be stored
 REPORTS_DIR=generated_reports
 
-YEAR=$(seq 2020 2030 | fzf)
+YEAR=$(seq 2021 2030 | fzf)
 MONTH=$(seq -w 1 12 | fzf)
 
 mkdir -p $REPORTS_DIR
@@ -18,4 +18,4 @@ python3 main.py \
   --month="$MONTH"  \
   --path-to-static-details=./my-static-data.yml > "$OUTPUT_FILENAME"
 
-echo "Report generated to $OUTPUT_FILENAME"
+echo "Report stored in $OUTPUT_FILENAME"
